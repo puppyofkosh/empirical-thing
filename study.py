a2c6444f06568426c594023e3518e9169887083e
ian boros <puppyofkosh@gmail.com>
2016-12-01 21:16:39 -0500

    More work on line-change finder

--- study.py
+++ study.py
@@ -7,10 +7,9 @@ import datetime
 
 #MIN_DATE = "October 1 2016"
 MIN_DATE = "November 1 2016"
-MAX_DATE = "November 15 2016"
 
 def get_recent_non_merge_commits():
-    args = ["git log  --pretty=oneline --no-color --since='{0}' --no-merges".format(MIN_DATE)]
+    args = ["git log --pretty=oneline --no-color --since='{0}' --no-merges".format(MIN_DATE)]
     stdout = run_command(args)
     lines = stdout.decode("utf-8").split("\n")
 
@@ -77,25 +76,27 @@ def find_lines_changed(commit):
 
 def get_line_history(commit, filename, start_line, end_line, min_date, max_date):
     print("Finding changes for L{1},{2}:{0}".format(filename, start_line, end_line))
-    args = "git log -L{0},{1}:{2} --since='{3}' --before='{4}' --no-merges --no-color --pretty=oneline {5}".format(
+    args = "git log -L{0},{1}:{2} --since='{3}' --pretty=oneline --format='commit:%H|%ad' {4}".format(
         start_line, end_line,
         filename,
-        min_date, max_date,
+        MIN_DATE,
         commit)
     output = run_command(args)
     lines = output.decode("utf-8").split("\n")
 
-    commits = []
-    bad_start_symbols = ["+", "-", "diff", "@@"]
+    commits = {}
     for l in lines:
-        if len(l) == 0 or any(l.startswith(b) for b in bad_start_symbols):
+        if len(l) == 0 or not l.startswith("commit:"):
             continue
 
         # The line should look something like:
         # a8348bca2944d397a528772f5c0ccb47a8b58af4 crypto: algif_hash - Fix whatever...
         # We want the hash
-        commit = l[:l.find(" ")]
-        commits.append(commit)
+        commit = l[l.find(":")+1:l.find("|")]
+        date = dateparser.parse(l[l.find("|")+1:])
+        if date >= min_date and date <= max_date:
+            commits[commit] = date
+
     return commits
 
 def main():
@@ -112,7 +113,7 @@ def main():
     for c in recent_commits:
         h = c['hash']
         # Find other changes to this line a week before
-        min_date = c['date'] - datetime.timedelta(days=7)
+        min_date = c['date'] - datetime.timedelta(days=14)
         # Ignore changes to this line on the same day (in case the bug was immediately fixed
         # or there were just a bunch of commits at once)
         max_date = c['date'] - datetime.timedelta(days=1)
@@ -121,22 +122,23 @@ def main():
         print("Changes are {0}".format(changes))
         for fname, line_changes in changes.items():
             for (start_line, end_line) in line_changes:
-                commits_list = get_line_history(h, fname, start_line, end_line,
-                                                min_date, max_date)
-                commits = set(commits_list)
+                commit_dict = get_line_history(h, fname, start_line, end_line,
+                                               min_date, max_date)
+                commit_hashes = set(commit_dict.keys())
 
-                hash_overlaps = bug_fix_hashes & commits
+                hash_overlaps = bug_fix_hashes & commit_hashes
                 for buggy_commit_hash in hash_overlaps:
-                    overlap_obj = {'fname': fname,
-                                   'start_line': start_line, 'end_line': end_line,
+                    overlap_obj = {'path': '{0},{1}:{2}'.format(start_line, end_line, fname),
                                    'fix_commit': c}
                     print(buggy_commit_hash)
-                    overlaps[buggy_commit_hash].append(overlap_obj)
+                    key = (buggy_commit_hash, commit_dict[buggy_commit_hash])
+                    overlaps[key].append(overlap_obj)
 
                 if len(hash_overlaps) > 0:
                     continue
 
     pprint.pprint(overlaps)
+    print("Total number of fixes since {0}:{1}".format(MIN_DATE, len(bug_fix_hashes)))
 
 if __name__ == "__main__":
     main()
